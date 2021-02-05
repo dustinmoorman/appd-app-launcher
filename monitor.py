d3f407a0488ebe5caac8f2ae2661bed968d79949
Dustin Moorman <dustin.moorman@appdynamics.com>
2021-02-05 12:17:46 -0600

    add initial python script

---
+++ monitor.py
@@ -0,0 +1,30 @@
+import sys
+import pyautogui
+import time
+import pygetwindow as gw
+import xml.etree.ElementTree as ET
+from pywinauto.keyboard import send_keys
+from pywinauto import application
+from pywinauto.application import Application
+from urllib.request import Request, urlopen, ssl, socket
+from urllib.error import URLError, HTTPError
+
+tree = ET.parse('monitor.xml')
+root = tree.getroot()
+
+targets = root.findall("./monitor-run-task/variables/targets/target")
+
+for app in targets:
+  appname = app.text
+
+  try:
+    app = application.Application()
+    app.start(appname)
+    # Do stuff to validate the application is working great
+    time.sleep(3)
+    app.kill()
+
+    DEFAULT_METRIC_PREFIX="name=Custom Metrics|AppLauncher|" + appname + "|Launch Status,value=" + str(daysLeft.days)
+    print(DEFAULT_METRIC_PREFIX)
+  except:
+    pass
